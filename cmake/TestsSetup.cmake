742831fa56ca51334d1abafcb6e64d7b8e5df869
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-11-11 14:44:35 +0300

    Hotfix: proper WD for tests

--- cmake/TestsSetup.cmake
+++ cmake/TestsSetup.cmake
@@ -7,13 +7,13 @@ include(GoogleTest)
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CMakeLists-GTest.txt.in googletest-download/CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
         RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/test/googletest-download )
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
 if(result)
     message(FATAL_ERROR "CMake step for googletest failed: ${result}")
 endif()
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
         RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/test/googletest-download )
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
 if(result)
     message(FATAL_ERROR "Build step for googletest failed: ${result}")
 endif()
