53d7d5dbe10935c0c782ea0785aa9e92e3666bf2
Viktor <klochkov44@gmail.com>
2020-11-27 10:45:05 +0100

    revert

---
+++ examples/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <random>
+
+#include <AnalysisTree/ToyMC.hpp>
+
+using namespace AnalysisTree;
+
+int main(int argc, char* argv[]) {
+
+  TaskManager* man = TaskManager::GetInstance();
+
+  auto* toy_mc = new ToyMC<std::default_random_engine>;
+  man->AddTask(toy_mc);
+
+  man->Init();
+  man->Run(10);
+  man->Finish();
+
+}
--- infra/Tree.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by vklochkov on 11/26/20.
-//
-
-#include "Tree.hpp"
