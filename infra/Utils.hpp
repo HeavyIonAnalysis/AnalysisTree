9a34b33787bba78502a9a2fe7e1d318f0b4a75d2
Viktor <klochkov44@gmail.com>
2021-02-03 12:38:15 +0100

    simplify the logic here

--- infra/Utils.hpp
+++ infra/Utils.hpp
@@ -6,30 +6,26 @@
 #define ANALYSISTREE_INFRA_UTILS_HPP
 
 #ifndef __has_include
-#define __has_include(MACRO) 0
+  #define __has_include(MACRO) 0
 #endif
 
-#if __has_include(<variant>)
-#include <variant>
-#if defined(__cpp_lib_variant)
-#define ANALYSISTREE_STD_VARIANT 1
-#define ANALYSISTREE_UTILS_VARIANT std::variant
-#define ANALYSISTREE_UTILS_VISIT std::visit
-#define ANALYSISTREE_UTILS_GET std::get
-#endif
-#endif
-
-#if __has_include(<boost/variant.hpp>)
-#define ANALYSISTREE_BOOST_VARIANT 1
+#if __has_include(<variant>) && defined(__cpp_lib_variant)
+  #include <variant>
+  #define ANALYSISTREE_STD_VARIANT 1
+  #define ANALYSISTREE_UTILS_VARIANT std::variant
+  #define ANALYSISTREE_UTILS_VISIT std::visit
+  #define ANALYSISTREE_UTILS_GET std::get
+#elif __has_include(<boost/variant.hpp>)
+  #define ANALYSISTREE_BOOST_VARIANT 1
 #endif
 
-#if !defined(ANALYSISTREE_STD_VARIANT) && defined(ANALYSISTREE_BOOST_VARIANT)
-#include <boost/variant.hpp>
-#include <boost/variant/static_visitor.hpp>
-#define ANALYSISTREE_BOOST_VARIANT 1
-#define ANALYSISTREE_UTILS_VARIANT boost::variant
-#define ANALYSISTREE_UTILS_VISIT boost::apply_visitor
-#define ANALYSISTREE_UTILS_GET boost::get
+#if defined(ANALYSISTREE_BOOST_VARIANT)
+  #include <boost/variant.hpp>
+  #include <boost/variant/static_visitor.hpp>
+  #define ANALYSISTREE_BOOST_VARIANT 1
+  #define ANALYSISTREE_UTILS_VARIANT boost::variant
+  #define ANALYSISTREE_UTILS_VISIT boost::apply_visitor
+  #define ANALYSISTREE_UTILS_GET boost::get
 #endif
 
 namespace AnalysisTree {
@@ -38,12 +34,12 @@ namespace Utils {
 
 template<typename RetType>
 struct Visitor
-#if !defined(ANALYSISTREE_STD_VARIANT)
+#if defined(ANALYSISTREE_BOOST_VARIANT)
     : public boost::static_visitor<RetType>
 #endif// USEBOOST
 {
  public:
-#if !defined(ANALYSISTREE_BOOST_VARIANT)
+#if defined(ANALYSISTREE_STD_VARIANT)
   typedef RetType result_type;
 #endif
   virtual ~Visitor() = default;
