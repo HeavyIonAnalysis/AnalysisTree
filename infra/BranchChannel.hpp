901c8054c2867b3ff4a5d5753bd14f72f4276e2e
Viktor <klochkov44@gmail.com>
2021-06-11 10:54:14 +0200

    compile + clang format

--- infra/BranchChannel.hpp
+++ infra/BranchChannel.hpp
@@ -5,58 +5,59 @@
 #ifndef ATTASKSKELETON_ATI2_BRANCHCHANNEL_HPP_
 #define ATTASKSKELETON_ATI2_BRANCHCHANNEL_HPP_
 
-#include "ATI2_fwd.hpp"
+#include "Field.hpp"
 #include "ValueHolder.hpp"
 
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
+namespace AnalysisTree {
 
-namespace ATI2 {
+class Branch;
+class BranchChannelsIter;
 
 class BranchChannel {
  public:
   /* Getting value */
-  inline ValueHolder Value(const Field &v) const {
+  inline ValueHolder Value(const Field& v) const {
     assert(v.GetParentBranch() == branch);
     assert(v.IsInitialized());
     return ValueHolder(v, data_ptr);
   }
-  inline ValueHolder operator[](const Field &v) const { return Value(v); };
+  inline ValueHolder operator[](const Field& v) const { return Value(v); };
   inline std::size_t GetNChannel() const { return i_channel; }
 
   /* usage of this functions is highly discouraged */
-  void *Data() { return data_ptr; }
-  const void *Data() const { return data_ptr; }
+  void* Data() { return data_ptr; }
+  const void* Data() const { return data_ptr; }
   template<typename T>
-  T *DataT() { return reinterpret_cast<T *>(data_ptr); }
+  T* DataT() { return reinterpret_cast<T*>(data_ptr); }
   template<typename T>
-  const T *DataT() const { return reinterpret_cast<T *>(data_ptr); }
+  const T* DataT() const { return reinterpret_cast<T*>(data_ptr); }
 
   /**
    * @brief Copy contents of other branch channel
    * @param other
    * @return
    */
-  void CopyContents(const BranchChannel &other);
-  void CopyContents(Branch &other);
+  void CopyContents(const BranchChannel& other);
+  void CopyContents(Branch& other);
 
-  void Print(std::ostream &os = std::cout) const;
+  void Print(std::ostream& os = std::cout) const;
 
  private:
   friend Branch;
   friend BranchChannelsIter;
 
-  BranchChannel(Branch *branch, std::size_t i_channel);
+  BranchChannel(Branch* branch, std::size_t i_channel);
   void UpdatePointer();
   void UpdateChannel(std::size_t new_channel);
 
-  void *data_ptr{nullptr};
-  Branch *branch;
+  void* data_ptr{nullptr};
+  Branch* branch;
   std::size_t i_channel;
 };
 
+}// namespace AnalysisTree
 
-}
-
-#endif //ATTASKSKELETON_ATI2_BRANCHCHANNEL_HPP_
+#endif//ATTASKSKELETON_ATI2_BRANCHCHANNEL_HPP_
