10c7441301454adbbf4f979b80a02a3c3936c7be
Viktor <klochkov44@gmail.com>
2021-07-23 18:42:14 +0200

    user example

---
+++ examples/UserTaskRead.cpp
@@ -0,0 +1,30 @@
+#include "UserTaskRead.hpp"
+
+#include <AnalysisTree/TaskManager.hpp>
+
+using namespace AnalysisTree;
+
+void UserTaskRead::Init() {
+
+  auto chain = TaskManager::GetInstance()->GetChain();
+
+  in_branches_.emplace("SimParticles");
+  in_branches_.emplace("RecTracks");
+
+  particles_ = chain->GetBranch("SimParticles");
+  tracks_ = chain->GetBranch("RecTracks");
+}
+
+void UserTaskRead::Exec() {
+  std::cout << particles_.size() << std::endl;
+  std::cout << tracks_.size() << std::endl;
+
+  auto pT = particles_.GetField("pT");
+  for(size_t i=0; i<particles_.size(); ++i){
+    auto particle = particles_[i];
+    std::cout << particle[pT] << std::endl;
+  }
+
+}
+
+UserTaskRead::~UserTaskRead() = default;
--- examples/UserTaskWrite.cpp
+++ examples/UserTaskWrite.cpp
@@ -1,3 +1,32 @@
 #include "UserTaskWrite.hpp"
 
+#include "AnalysisTree/TaskManager.hpp"
+
 using namespace AnalysisTree;
+
+void UserTaskWrite::Init() {
+  auto man = TaskManager::GetInstance();
+  auto chain = man->GetChain();
+
+  in_branches_.emplace("SimParticles");
+  particles_ = chain->GetBranch("SimParticles");
+
+  auto br_conf = chain->GetConfiguration()->GetBranchConfig("SimParticles");
+  auto new_conf = br_conf.Clone("NewParticles", DetType::kParticle);
+  auto new_conf1 = br_conf.Clone("NewParticles1", DetType::kParticle);
+
+  man->AddBranch(new_particles_ptr_, new_conf);
+
+  new_particles_ = Branch(new_conf1);
+  new_particles_.SetMutable();
+  man->AddBranch(&new_particles_);
+}
+
+void UserTaskWrite::Exec() {
+  *new_particles_ptr_ = *(particles_.GetDataRaw<Particles*>());
+  new_particles_.CopyContentsRaw(&particles_);
+}
+
+UserTaskWrite::~UserTaskWrite() {
+  delete new_particles_ptr_;
+}
