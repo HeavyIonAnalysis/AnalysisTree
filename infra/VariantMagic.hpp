3f3efccd888383b6a07c0088a68b024c39cdb152
Viktor <klochkov44@gmail.com>
2021-07-28 11:14:04 +0200

    c++11 compilation

--- infra/VariantMagic.hpp
+++ infra/VariantMagic.hpp
@@ -25,12 +25,61 @@ namespace AnalysisTree {
 //  int i_channel_;
 //};
 
-//struct get_id_struct : public Utils::Visitor<int> {
-//  template<typename Entity>
-//  int get_id(Entity* d) const { return d->GetId(); }
-//  template<typename Entity>
-//  int operator()(Entity* d) const { return get_id<Entity>(d); }
-//};
+struct get_id_struct : public Utils::Visitor<size_t> {
+  template<typename Entity>
+  size_t get_id(Entity* d) const { return d->GetId(); }
+  template<typename Entity>
+  size_t operator()(Entity* d) const { return get_id<Entity>(d); }
+};
+
+struct get_channel_struct : public Utils::Visitor<ChannelPointer> {
+  explicit get_channel_struct(size_t i) : i_channel_(i) {}
+  template<typename Entity>
+  ChannelPointer get_channel(Entity* d) const { return ChannelPointer( &(d->Channel(i_channel_)) ); }
+  template<typename Entity>
+  ChannelPointer operator()(Entity* d) const { return get_channel<Entity>(d); }
+  size_t i_channel_;
+};
+
+struct clear_channels_struct : public Utils::Visitor<void> {
+  template<typename Entity>
+  void clear_channels(Entity* d) const { d->ClearChannels(); }
+  template<typename Entity>
+  void operator()(Entity* d) const { clear_channels<Entity>(d); }
+};
+
+struct new_channel_struct : public Utils::Visitor<void> {
+  explicit new_channel_struct(BranchConfig* config) : config_(config) {}
+  template<typename Entity>
+  void new_channel(Entity* d) const {
+    auto channel = d->AddChannel();
+    channel->Init(*config_);
+  }
+  template<typename Entity>
+  void operator()(Entity* d) const { new_channel<Entity>(d); }
+  BranchConfig* config_;
+};
+
+template<typename T>
+struct get_field_struct : public Utils::Visitor<double> {
+  explicit get_field_struct(int id) : id_(id) {}
+  template<typename Entity>
+  double get_field(Entity* d) const { return d->template GetField<T>(id_); }
+  template<typename Entity>
+  double operator()(Entity* d) const { return get_field<Entity>(d); }
+  int id_{-999};
+};
+
+template<typename T>
+struct set_field_struct : public Utils::Visitor<void> {
+  set_field_struct(double value, int id) : value_(value), id_(id) {}
+  template<typename Entity>
+  void set_field(Entity* d) const { d->template SetField<T>(value_, id_); }
+  template<typename Entity>
+  void   operator()(Entity* d) const { set_field<Entity>(d); }
+  double value_{0.};
+  int id_{-999};
+};
 
 //struct apply_cut : public Utils::Visitor<bool> {
 //  apply_cut(int i_channel_, const Cuts* cut) : i_channel_(i_channel_), cut_(cut) {}
@@ -64,12 +113,12 @@ namespace AnalysisTree {
 //  int ch2_;
 //};
 
-//struct get_n_channels_struct : public Utils::Visitor<size_t> {
-//  template<class Det>
-//  size_t get_n_channels(Det* d) const { return d->GetNumberOfChannels(); }
-//  template<typename Entity>
-//  size_t operator()(Entity* d) const { return get_n_channels<Entity>(d); }
-//};
+struct get_n_channels_struct : public Utils::Visitor<size_t> {
+  template<class Det>
+  size_t get_n_channels(Det* d) const { return d->GetNumberOfChannels(); }
+  template<typename Entity>
+  size_t operator()(Entity* d) const { return get_n_channels<Entity>(d); }
+};
 
 struct set_branch_address_struct : public Utils::Visitor<int> {
   set_branch_address_struct(TTree* tree, std::string name) : tree_(tree), name_(std::move(name)) {}
