f0ccbabd612e8fa8ab3013c0ac44e78bbaf971d2
viktorklochkov <viktorklochkov@users.noreply.github.com>
2022-01-21 21:35:08 +0000

    deploy: 474a2c3aa94d4265c6442cd1ba456e2517621c6b

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['copycontent_0',['CopyContent',['../da/d13/classAnalysisTree_1_1BranchChannel.html#aa363cdfc8643a5d9831a39e8e213a8ac',1,'AnalysisTree::BranchChannel']]],
-  ['copycontentsraw_1',['CopyContentsRaw',['../d2/d4d/classAnalysisTree_1_1Branch.html#a43bbda1cbcdced76fedc7322b053e116',1,'AnalysisTree::Branch']]]
+  ['clone_0',['Clone',['../d8/d4c/classAnalysisTree_1_1BranchConfig.html#a12d619a961e62992d32c620a5ad280bf',1,'AnalysisTree::BranchConfig']]],
+  ['copycontent_1',['CopyContent',['../da/d13/classAnalysisTree_1_1BranchChannel.html#aa363cdfc8643a5d9831a39e8e213a8ac',1,'AnalysisTree::BranchChannel']]],
+  ['copycontentsraw_2',['CopyContentsRaw',['../d2/d4d/classAnalysisTree_1_1Branch.html#a43bbda1cbcdced76fedc7322b053e116',1,'AnalysisTree::Branch']]]
 ];
