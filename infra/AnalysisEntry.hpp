7bba099b3869f0169856feb223be675856523203
Oleksii Lubynets <lubinets95@gmail.com>
2024-02-13 16:17:10 +0100

    fix bug when weights_ is empty var

--- infra/AnalysisEntry.hpp
+++ infra/AnalysisEntry.hpp
@@ -29,6 +29,9 @@ class AnalysisEntry {
   explicit AnalysisEntry(std::vector<Variable> vars, Cuts* cuts = nullptr, Variable vars4weight={}) : vars_(std::move(vars)),
                                                                                                       var4weight_(std::move(vars4weight)),
                                                                                                       cuts_(cuts) {
+    if(var4weight_.GetNumberOfBranches() == 0) {
+      var4weight_ = Variable::FromString(vars_.at(0).GetBranchName() + ".ones");
+    }
     FillBranchNames();
   };
 
