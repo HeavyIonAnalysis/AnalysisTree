77f863aee074b1b7b76deb078626ea52217085b1
Oleksii Lubynets <66421517+lubynets@users.noreply.github.com>
2024-07-23 11:36:06 +0200

    save diff if present since last commit (#127)

--- cmake/AnalysisTreeHashWriter.sh
+++ cmake/AnalysisTreeHashWriter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FILE=AnalysisTreeHash.sh
-if [ -f $FILE ]; then
-rm $FILE
+FILE_HASH=AnalysisTreeHash.sh
+FILE_DIFF=AnalysisTreeDiff.patch
+if [ -f $FILE_HASH ]; then
+rm $FILE_HASH
 fi
 
 SRC_DIR=${1}
@@ -12,14 +13,15 @@ if [ -d ".git" ]; then
 GITCOMMIT=$(git rev-parse HEAD)
 GITSTATUS=$(git status --porcelain)
 cd -
-echo "export ANALYSIS_TREE_COMMIT_HASH=${GITCOMMIT}" >> $FILE
+echo "export ANALYSIS_TREE_COMMIT_HASH=${GITCOMMIT}" >> $FILE_HASH
 if [ -z "${GITSTATUS}" ]; then
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=TRUE" >> $FILE
+echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=TRUE" >> $FILE_HASH
 else
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=FALSE" >> $FILE
+echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=FALSE" >> $FILE_HASH
+git diff >> $FILE_DIFF
 fi
 else
 cd -
-echo "export ANALYSIS_TREE_COMMIT_HASH=NOT_A_GIT_REPO" >> $FILE
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=NOT_A_GIT_REPO" >> $FILE
+echo "export ANALYSIS_TREE_COMMIT_HASH=NOT_A_GIT_REPO" >> $FILE_HASH
+echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=NOT_A_GIT_REPO" >> $FILE_HASH
 fi
