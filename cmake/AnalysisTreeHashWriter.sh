16ebadc8a7f221382d35675c214577134c1be4ae
Oleksii Lubynets <lubinets95@gmail.com>
2024-08-05 17:45:04 +0200

    fix error with git diff in AnalysisTreeHashWriter.sh

--- cmake/AnalysisTreeHashWriter.sh
+++ cmake/AnalysisTreeHashWriter.sh
@@ -2,29 +2,31 @@
 
 FILE_HASH=AnalysisTreeHash.sh
 FILE_DIFF=AnalysisTreeDiff.patch
-if [ -f $FILE_HASH ]; then
-rm $FILE_HASH $FILE_DIFF
-fi
+for F in $FILE_HASH $FILE_DIFF; do
+  if [ -f $F ]; then
+    rm $F
+  fi
+done
 
 SRC_DIR=${1}
 
 cd $SRC_DIR
 if [ -d ".git" ]; then
-GITTAG=$(git describe --tags)
-GITCOMMIT=$(git rev-parse HEAD)
-GITSTATUS=$(git status --porcelain)
-cd -
-echo "export ANALYSIS_TREE_TAG=\"${GITTAG}\"" >> $FILE_HASH
-echo "export ANALYSIS_TREE_COMMIT_HASH=${GITCOMMIT}" >> $FILE_HASH
-if [ -z "${GITSTATUS}" ]; then
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=TRUE" >> $FILE_HASH
+  GITTAG=$(git describe --tags)
+  GITCOMMIT=$(git rev-parse HEAD)
+  GITSTATUS=$(git status --porcelain)
+  cd -
+  echo "export ANALYSIS_TREE_TAG=\"${GITTAG}\"" >> $FILE_HASH
+  echo "export ANALYSIS_TREE_COMMIT_HASH=${GITCOMMIT}" >> $FILE_HASH
+  if [ -z "${GITSTATUS}" ]; then
+    echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=TRUE" >> $FILE_HASH
+  else
+    echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=FALSE" >> $FILE_HASH
+    git diff >> $FILE_DIFF
+  fi
 else
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=FALSE" >> $FILE_HASH
-fi
-git diff >> $FILE_DIFF
-else
-cd -
-echo "export ANALYSIS_TREE_TAG=NOT_A_GIT_REPO" >> $FILE_HASH
-echo "export ANALYSIS_TREE_COMMIT_HASH=NOT_A_GIT_REPO" >> $FILE_HASH
-echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=NOT_A_GIT_REPO" >> $FILE_HASH
+  cd -
+  echo "export ANALYSIS_TREE_TAG=NOT_A_GIT_REPO" >> $FILE_HASH
+  echo "export ANALYSIS_TREE_COMMIT_HASH=NOT_A_GIT_REPO" >> $FILE_HASH
+  echo "export ANALYSIS_TREE_COMMIT_ORIGINAL=NOT_A_GIT_REPO" >> $FILE_HASH
 fi
