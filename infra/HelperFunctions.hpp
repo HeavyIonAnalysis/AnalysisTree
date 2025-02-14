867053afe7226db9c8f58f3d2d2c9993354f6ecc
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-02-14 15:33:00 +0100

    add helper functions

--- infra/HelperFunctions.hpp
+++ infra/HelperFunctions.hpp
@@ -16,11 +16,34 @@ inline std::string ToStringWithPrecision(const T a_value, const int n) {
   return out.str();
 }
 
-inline std::vector<AnalysisTree::SimpleCut> CreateSliceCuts(const std::vector<float>& ranges, const std::string& cutNamePrefix, const std::string& branchFieldName) {
+template<typename T>
+inline std::string ToStringWithSignificantFigures(const T a_value, const int n) {
+  if(a_value == 0) return "0";
+
+  const double dMag = std::log10(std::abs(a_value)); // scale of the a_value (e.g 1.* for 1.2345, 2.* for 12.345 etc)
+  const int iMag = static_cast<int>(dMag-n+1 > 0 ? dMag-n+1 : dMag-n);
+  const T shifted_value = a_value/std::pow(10, iMag); // shift decimal point to have all required digits to l.h.s. from it
+  const T rounded_value = static_cast<T>(std::round(shifted_value)); // get rid of r.h.s. from decimal point
+  const T reshifted_value = rounded_value*std::pow(10, iMag); // return decimal point to its original place
+  const int precision = iMag < 0 ? -iMag : 0; // determine how many digits after decimal point one needs
+  return ToStringWithPrecision(reshifted_value, precision);
+}
+
+inline std::vector<AnalysisTree::SimpleCut> CreateRangeCuts(const std::vector<float>& ranges, const std::string& cutNamePrefix, const std::string& branchFieldName, int precision=2) {
   std::vector<AnalysisTree::SimpleCut> sliceCuts;
   for (int iRange = 0; iRange < ranges.size() - 1; iRange++) {
-    const std::string cutName = cutNamePrefix + ToStringWithPrecision(ranges.at(iRange), 2) + "_" + ToStringWithPrecision(ranges.at(iRange + 1), 2);
+    const std::string cutName = cutNamePrefix + ToStringWithPrecision(ranges.at(iRange), 2) + "_" + ToStringWithPrecision(ranges.at(iRange + 1), precision);
     sliceCuts.emplace_back(AnalysisTree::RangeCut(branchFieldName, ranges.at(iRange), ranges.at(iRange + 1), cutName));
+  }
+
+  return sliceCuts;
+}
+
+inline std::vector<AnalysisTree::SimpleCut> CreateEqualCuts(const std::vector<float>& values, const std::string& cutNamePrefix, const std::string& branchFieldName, int precision=2) {
+  std::vector<AnalysisTree::SimpleCut> sliceCuts;
+  for (int iValue = 0; iValue < values.size(); iValue++) {
+    const std::string cutName = cutNamePrefix + ToStringWithPrecision(values.at(iValue), precision);
+    sliceCuts.emplace_back(AnalysisTree::EqualsCut(branchFieldName, values.at(iValue), cutName));
   }
 
   return sliceCuts;
