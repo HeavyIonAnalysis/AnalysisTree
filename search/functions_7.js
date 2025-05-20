7bed9ffdea4dd0d463016ec1a45b6f6bb498d5b0
lubynets <lubynets@users.noreply.github.com>
2025-05-20 08:51:10 +0000

    deploy: 2b64796d87b80be6d5924ece927f00a3bf0032f5

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_3d_3d_0',['operator==',['../d9/d26/namespaceAnalysisTree.html#a378da65a3ae1fdfda6b772eb8a1b2c1e',1,'AnalysisTree']]]
+  ['opencut_0',['OpenCut',['../d9/d26/namespaceAnalysisTree.html#af048ed5f34520fd35eb91122f14a6b19',1,'AnalysisTree']]],
+  ['operator_3d_3d_1',['operator==',['../d9/d26/namespaceAnalysisTree.html#a378da65a3ae1fdfda6b772eb8a1b2c1e',1,'AnalysisTree']]]
 ];
--- search/related_1.js
+++ search/related_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_3d_3d_0',['operator==',['../da/d0a/classAnalysisTree_1_1Track.html#a6a3c675fdd23ff49d2670ffe5727bd67',1,'AnalysisTree::Track']]]
+  ['opencut_0',['OpenCut',['../d4/dfc/classAnalysisTree_1_1SimpleCut.html#a5d05227b85187aa99d76515061e57cf1',1,'AnalysisTree::SimpleCut']]],
+  ['operator_3d_3d_1',['operator==',['../da/d0a/classAnalysisTree_1_1Track.html#a6a3c675fdd23ff49d2670ffe5727bd67',1,'AnalysisTree::Track']]]
 ];
